b66030c584e878a6072e125ccaec040d405b4b39
jonraf <jonmrafoss@gmail.com>
2019-09-16 11:38:16 +0200

    hhh

--- src/Oblig1.java
+++ src/Oblig1.java
@@ -1,3 +1,5 @@
+import java.util.Arrays;
+
 public class Oblig1 {
 
     // oppgave 1
@@ -70,11 +72,72 @@ public class Oblig1 {
     }
 
 
+    // oppgave 5
+    public static void byttChar (char[] a, int b, int c){
+        char mt = a[b];
+        a[b] = a[c];
+        a[c] = mt;
+    }
+
+    // oppgave 5
+    public static void rotasjon(char[] a){
+        char temp = a[a.length-1];
+
+
+        for (int i  = a.length-1; i >0; i--){
+            a[i] = a[i-1];
+        }
+
+        a[0] = temp;
+
+
+
+
+    }
+
+    // oppgave 6
+    public static void rotasjonBakover(char[] a){
+        char temp = a[0];
+
+
+        for (int i  = 0; i < a.length-1; i++){
+            a[i] = a[i+1];
+        }
+
+        a[a.length-1] = temp;
+
+
+    }
+
+    //oppgave 6
+
+    public static void rotasjon(char[] a, int k){
+
+        if (k < 0){
+            for (int i = 0; i < k; i++) {
+                rotasjonBakover(a);
+            }
+        }
+        else {
+
+            for (int i = 0; i < k; i++) {
+                rotasjon(a);
+            }
+        }
+    }
+
+  
+
 
 
     public static void main (String[] args){
-        int[] a = {5, 3, 7, 4, 3, 5, 7, 8, 7, 7};
-        System.out.println(antallUlikeUsortert(a));
+       // int[] a = {6,10,9,4,1,3,8,5,2,7};
+        char[] charArray = { 'a', 'b', 'c', 'd', 'e', 'f','g','h','i','j' };
+        rotasjonBakover(charArray);
+
+        //System.​out​.println(Arrays.​toString(​charArray));
+        //delsortering(a);
+        //System.out.println(a.length);
     }
 
 
